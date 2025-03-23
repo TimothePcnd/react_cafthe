34d7b78953f366b1c16b952acb3c0396046b83df
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-03-24 00:55:58 +0100

    Accessory.js : Structuration de la page, duplication de la page Tea.js. Accessory.css : ajout du css

--- src/pages/Accessory.js
+++ src/pages/Accessory.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import Skeleton from "react-loading-skeleton";
 import ProductCard from "../components/ProductCard";
+import '../styles/Accessory.css'
 
 
 function Accessory(props) {
@@ -11,7 +12,7 @@ function Accessory(props) {
     useEffect(() => {
         const fetchAccessories = async () => {
             try {
-                const response = await axios.get("http://localhost:3001/api/produits/accessoires");
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/produits/accessoires`);
                 setAccessories(response.data);
             } catch (error) {
                 console.error("Erreur de chargement des produits", error);
@@ -49,15 +50,27 @@ function Accessory(props) {
 
     return (
         <div>
-            <h3>Liste des accessoires</h3>
-            <div className={"product-list"}>
-                {accessories.map((produit) => (
-                    <div>
-                        <ProductCard key={produit.id_produit} produit={produit} />
-                    </div>
-                ))}
+            <div className={"top-pageAccessories"}>
+                <div className={"white"}><p>NOS ACCESSOIRES</p></div>
+                <div className={"orange"}><p>NOS ACCESSOIRES</p></div>
+                <div className={"black"}><p>NOS ACCESSOIRES</p></div>
 
             </div>
+
+            <div className={"middle-page"}>
+                <div className={"filtre-content"}>
+                    <h3>Trier par</h3>
+                </div>
+
+                <div className={"product-list"}>
+                    {accessories.map((produit) => (
+                        <div>
+                            <ProductCard key={produit.id_produit} produit={produit}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
         </div>
     );
 }
