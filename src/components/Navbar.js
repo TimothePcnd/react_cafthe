7c85b310b5f0aa7a81b6766e1ea01dac820a14bc
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-03-23 22:17:34 +0100

    Mise à jour de Navbar.css et de Navbar.js en ajoutant des classes au éléments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,28 +2,31 @@ import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
 import '../styles/Navbar.css';
 import {AuthContext} from "../context/AuthContext";
+import {CartContext} from "../context/CartContext";
 
 
 function Navbar(props) {
     const {user, isAuthenticated, logout} = useContext(AuthContext);
+    const {items} = useContext(CartContext)
     const handleLogout = () => {
         logout();
     }
 
     return (
-            <nav className="nav-bar">
+            <nav className="navbar">
 
                 <ul className="produit">
-                    <li><Link to={`/produits/cafes`}>Café</Link></li>
-                    <li><Link to={`/produits/thes`}>Thé</Link></li>
-                    <li><Link to={`/produits/accessoires`}>Accessoires</Link></li>
+                    <li className={"li-navbar"}><Link to={`/produits/cafes`}>Café</Link></li>
+                    <li className={"li-navbar"}><Link to={`/produits/thes`}>Thé</Link></li>
+                    <li className={"li-navbar"}><Link to={`/produits/accessoires`}>Accessoires</Link></li>
                 </ul>
 
-                <div className={"title-nav"}><Link to={`/`}></Link></div>
+                <div className={"title-nav"}>
+                    <Link to={`/`}><img src={"cafthe-title.png"} height={"35px"}></img></Link>
+                </div>
 
                 <ul className="right-ul">
-                    <li>Search</li>
-                    <li><Link to="/Panier" className="link-nav">Panier</Link></li>
+                    <li className={"li-navbar"}><Link to="/Panier" className="link-nav">Panier ({items.length})</Link></li>
                     {isAuthenticated ? (
                         <>
                             <button className="btn-logout"><Link to={"/account"}>Mon compte</Link></button>
