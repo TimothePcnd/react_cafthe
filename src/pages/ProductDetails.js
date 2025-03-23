0af2aa7e73c71429e652b38cb1965c518a662b6c
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-03-24 00:44:47 +0100

    ProductDetails.js : ajout de code et structuration du code. ProductDetails.css : ajout du css

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,15 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import {Link, useParams} from "react-router-dom";
 import axios from "axios";
+import '../styles/ProductDetails.css'
+import ProductCard from "../components/ProductCard";
 
 function ProductDetails() {
         const {id} = useParams();
         const [produit, setProduit] = useState([]);
 
+
     useEffect(() => {
         const fetchProduits = async () => {
             try {
-                const response = await axios.get(`http://localhost:3001/api/produit/${id}`);
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/produit/${id}`);
                 setProduit(response.data);
             } catch (error) {
                 console.error("Erreur de chargement du produit", error);
@@ -21,12 +24,39 @@ function ProductDetails() {
 
     return (
         <div className={"product-details"}>
-            {/*Img*/}
-            <h3>Désignation : {produit.designation_produit}</h3>
-            <p>Prix : {produit.prix_ht_produit} €</p>
-            <p>Description : {produit.description_produit}</p>
-            <p>Conditionnement : {produit.Type_conditionnement}</p>
             <Link to={`/`} className={"details-btn"}>Retour</Link>
+
+        <div className={"container-topProduit"}>
+            <div className={"wrapper"}>
+                <div className={"detail-section1"}>
+
+                    <div className={"img-produitMini"}>
+                        <img src={"cafeSachet.png"} alt={"photo du produit"}></img>
+                        <img src={"cafeSachet.png"} alt={"photo du produit"}></img>
+                        <img src={"cafeSachet.png"} alt={"photo du produit"}></img>
+                    </div>
+
+                    <div className={"description-produit"}>
+                        <h4>Description</h4>
+                        <p>{produit.description_produit}</p>
+                    </div>
+                </div>
+
+                <div className={"detail-section2"}>
+                    <img className={"img-product"} src={"cafeSachet.png"} alt={"photo du produit"}/>
+                </div>
+
+                <div className={"detail-section3"}>
+                    <h3>{produit.designation_produit}</h3>
+                    <p>Prix : {produit.prix_ht_produit} €</p>
+                    <p>Conditionnement : {produit.Type_conditionnement}</p>
+                    <button>Ajouter au panier</button>
+                </div>
+            </div>
+
+        </div>
+
+
         </div>
     );
 }
