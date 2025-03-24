371237bee7817156c5d182110a0946e4b2fad548
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-03-24 01:29:15 +0100

    Effacement du Header.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet} from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProductList from "../pages/ProductList";
 
@@ -9,7 +8,6 @@ function Layout(props) {
     return (
         <>
             <Navbar />
-            <Header />
             {/* Outlet : là où s'affichent les pages enfants*/}
             <Outlet />
             <Footer />
