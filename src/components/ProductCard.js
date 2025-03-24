f6bfe91403c9356dc6855160539e8ef4216f060d
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-03-24 01:33:49 +0100

    Ajout d'une img et ajout de la function ajout au panier

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
+import {CartContext} from "../context/CartContext";
 
+function ProductCard({ produit }) {
+    const { addItemToCart } = useContext(CartContext);
 
-
-function ProductCard({produit}) {
     return (
-            <div className={"product-card"}>
-                {/*Img*/}
-                <h3>{produit.designation_produit}</h3>
-                <p>{produit.prix_ht_produit} €</p>
-                    <Link to={`/produit/${produit.id_produit}`} className={"details-btn"}>Voir détails</Link>
-            </div>
-);
-
+        <div className="product-card">
+            <img src={"cafeSachet.png"} alt={"image produit"}/>
+            <h3>{produit.designation_produit}</h3>
+            <p>{produit.prix_ht_produit} €</p>
+            <Link to={`/produit/${produit.id_produit}`} className="details-btn">
+                Voir détails
+            </Link>
+            <button onClick={() => addItemToCart(produit.id_produit)}>Ajouter au panier</button>
+        </div>
+    );
 }
 
 export default ProductCard;
