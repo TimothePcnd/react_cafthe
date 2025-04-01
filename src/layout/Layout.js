09d42ca296431cb11541d8a5ccd708ec78e230b6
timothe.pecnard <timothe.pecnard@campus-centre.fr>
2025-04-01 12:06:59 +0200

    Changement nom import

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Outlet} from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ProductList from "../pages/ProductList";
 
 function Layout(props) {
     return (
         <>
-            <Navbar />
+            <Header />
             {/* Outlet : là où s'affichent les pages enfants*/}
             <Outlet />
             <Footer />
